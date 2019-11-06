4e0c0112933227195cb72c2454dbabb6fd0c9a2f
Tal TKMedia MacBook 2017 <tal@tkmedia.co.il>
2019-11-06 11:55:03 +0200

    Update flexible-qa-accordion.php

--- partials/flexible-layouts/flexible-qa-accordion.php
+++ partials/flexible-layouts/flexible-qa-accordion.php
@@ -1,19 +1,25 @@
 <?php 
-$qa_accordion_block_width = get_sub_field('qa_acc_block_width');
-$qa_accordion_order = get_sub_field('qa_acc_order');
-$qa_accordion_mobile = get_sub_field('qa_acc_mobile');
-$qa_accordion_hide_mobile = get_sub_field('qa_acc_hide_mobile');
-$qa_accordion_break = get_sub_field('qa_acc_break');
-$qa_accordion_block_align = get_sub_field('qa_acc_block_align');
+$qa_accordion_block_width = get_sub_field('qaa_bW');
+$qa_accordion_order = get_sub_field('qaa_or');
+$qa_accordion_mobile = get_sub_field('qaa_mo');
+$qa_accordion_hide_mobile = get_sub_field('qaa_hMo');
+$qa_accordion_break = get_sub_field('qaa_br');
+$qa_accordion_block_align = get_sub_field('qaa_bAl');
 
-$qa_accordion_title = get_sub_field('qa_acc_title');
-$page_qa_subtitle = get_sub_field('flex_page_qa_subtitle');
-$qa_accordion_style = get_sub_field('qa_acc_style');
+$qa_accordion_title = get_sub_field('qaa_t');
+$qa_acc_subtitle = get_sub_field('qaa_st');
+$qa_accordion_style = get_sub_field('qaa_sy');
 $page_qa_title_a = get_sub_field('qa_acc_title_a');
-$qa_accordion_cols = get_sub_field('qa_acc_cols');
-$qa_accordion_icon = get_sub_field('qa_acc_icon');
+$qa_accordion_cols = get_sub_field('qaa_col');
+$qa_accordion_icon = get_sub_field('qaa_ic');
 $qa_accordion = get_sub_field('qa_acc');
-$qa_accordion_animation = get_sub_field('qa_acc_animation');
+$qa_accordion_animation = get_sub_field('qaa_an');
+
+$qa_btn = get_sub_field('qaa_bt');
+$qa_link_type = get_sub_field('qaa_lTy');
+$qa_link = get_sub_field('qaa_lk');
+$qa_page_link = get_sub_field('qaa_pL');
+$qa_form_link = get_sub_field('qaa_fL');
 
 if ( $qa_accordion_hide_mobile && wp_is_mobile() ) {
 //HIDE ON MOBILE
@@ -25,21 +31,23 @@ if ( $qa_accordion_hide_mobile && wp_is_mobile() ) {
 		<div class="content_page_qa flexible_page_element qa_icon_<?php echo $qa_accordion_icon; ?>" itemprop="text">
 			<div class="page_qa_wrap style_arrow">
 				<?php if( $qa_accordion_title ) { ?>
-				<h2 class="section_title section_flex_title title_<?php echo $page_qa_title_a; ?>" style="text-align:<?php echo $page_qa_title_a; ?> !important;"><?php echo $qa_accordion_title; ?></h2>
-				<?php } ?>				
-				<?php if( $page_qa_subtitle ) { ?>
-				<div class="page_qa_subtitle title_<?php echo $page_qa_title_a; ?>" style="text-align:<?php echo $page_qa_title_a; ?> !important;"><p><?php echo $page_qa_subtitle; ?></p></div>
+				<div class="page_qa_title_wrap">
+					<h2 class="section_title section_flex_title title_<?php echo $page_qa_title_a; ?>" style="text-align:<?php echo $page_qa_title_a; ?> !important;"><?php echo $qa_accordion_title; ?></h2>
+					<?php if( $qa_acc_subtitle ) { ?>
+					<div class="page_qa_subtitle title_<?php echo $page_qa_title_a; ?>" style="text-align:<?php echo $page_qa_title_a; ?> !important;"><p><?php echo $qa_acc_subtitle; ?></p></div>
+					<?php } ?>
+				</div>
 				<?php } ?>				
 
 				<?php if( $qa_accordion_style == 'tabs' ) { ?>		
 
 					<!-- Begin .qa_tabs -->
 					<div class="VerticalTab fc_VerticalTab VerticalTab_1 tabs_ver_1 row-flex">
-					<?php if( have_rows('qa_acc') ): ?>
+					<?php if( have_rows('qaa_r') ): ?>
 						<ul class="resp-tabs-list hor_1 col-sm-5">
-						<?php $i = 1; while( have_rows('qa_acc') ): the_row(); 
+						<?php $i = 1; while( have_rows('qaa_r') ): the_row(); 
 							// vars
-							$qa_tabs_q = get_sub_field('qa_acc_question');
+							$qa_tabs_q = get_sub_field('qaa_rT');
 							?>								
 							<li class="tabs-<?php echo $i; ?>">
 								<span class="tabs-text">
@@ -49,15 +57,15 @@ if ( $qa_accordion_hide_mobile && wp_is_mobile() ) {
 						<?php $i++;endwhile; ?>
 						</ul>
 						<?php endif; ?>
-						<?php if( have_rows('qa_acc') ): ?>	
+						<?php if( have_rows('qaa_r') ): ?>	
 						<div class="resp-tabs-container hor_1 col-sm-7">
-						<?php $i = 1; while( have_rows('qa_acc') ): the_row();
+						<?php $i = 1; while( have_rows('qaa_r') ): the_row();
 							// vars
-							$qa_tabs_a = get_sub_field('qa_acc_answer');
+							$qa_tabs_a = get_sub_field('qaa_rA');
 							?>								
 							<div class="tabcontent fc-tab-<?php echo $i; ?>">
 								<?php if( $qa_accordion_icon == 'text_qa' ) { ?>
-								<div class="title_contanier"><?php _e('Answer', 'tkmnineteen'); ?></div>
+								<div class="title_contanier"><?php _e('Answer', 'tkmulti'); ?></div>
 								<?php } ?>
 								<div class="tabcontent_contanier"><?php echo $qa_tabs_a; ?></div>
 							</div>
@@ -69,23 +77,23 @@ if ( $qa_accordion_hide_mobile && wp_is_mobile() ) {
 				<?php } ?>
 				<?php
 				if( $qa_accordion_style == 'accordion' ) { ?>				
-					<?php if( have_rows('qa_acc') ): ?>
+					<?php if( have_rows('qaa_r') ): ?>
 						<div class="page_qa_row page_qa_row_accordion">
 							<div class="page_qa_col row-flex">
-								<?php $i = 1; while (have_rows('qa_acc')) : the_row();
-									$accordion_question = get_sub_field('qa_acc_question');
-									$accordion_answer = get_sub_field('qa_acc_answer'); ?>
+								<?php $i = 1; while (have_rows('qaa_r')) : the_row();
+									$accordion_question = get_sub_field('qaa_rT');
+									$accordion_answer = get_sub_field('qaa_rA'); ?>
 								<div class="page_qa_item <?php if( $qa_accordion_cols == 'page_qa_two_col' ) { ?>page_qa_two_item<?php } else {?>page_qa_one_item<?php } ?>">
 									<div id="question-<?php echo $i; ?>" aria-controls="answer-<?php $i; ?>" role="tab" class="page_qa_item_question">
 										<?php if( $qa_accordion_icon == 'text_qa' ) { ?>
-										<div class="accordion_question_pre"><?php _e('Question', 'tkmnineteen'); ?></div>
+										<div class="accordion_question_pre"><?php _e('Question', 'tkmulti'); ?></div>
 										<?php } ?>
 										<div class="accordion_question"><?php echo $accordion_question; ?></div>
 									</div>
 									<div id="answer-<?php $i; ?>" aria-labelledby="question-<?php $i; ?>" class="page_qa_item_answer">
 										<div class="accordion_answer">
 										<?php if( $qa_accordion_icon == 'text_qa' ) { ?>
-										<div class="accordion_answer_pre"><?php _e('Answer', 'tkmnineteen'); ?></div>
+										<div class="accordion_answer_pre"><?php _e('Answer', 'tkmulti'); ?></div>
 										<?php } ?>
 										<div class="accordion_answer_wrap"><?php echo $accordion_answer; ?></div>
 										</div>
@@ -98,8 +106,57 @@ if ( $qa_accordion_hide_mobile && wp_is_mobile() ) {
 					
 				<?php } ?>					
 			</div>
+			<?php if( $qa_btn ) { ?>
+			<div class="page_qa_btn section_readmore_link_wrap">
+				<?php if( $qa_link_type == 'page_link' ): ?>
+				<a href="<?php echo $qa_page_link; ?>">
+				<?php endif; ?>	
+				<?php if( $qa_link_type == 'free_link' ): ?>
+				<a href="<?php echo $qa_link; ?>">
+				<?php endif; ?>
+				<?php if( $qa_link_type == 'form_link' ): ?>					
+				<a data-fancybox data-src="#popop-form-<?php echo $row;?>-<?php echo $count;?>" href="javascript:;">
+				<?php endif; ?>	
+				<button class="section_readmore_link watch_btn hoverLink"><?php echo $qa_btn; ?></button>
+				</a>
+			</div>
+			<?php } ?>
+
 
 		</div>
+		
+		<?php if( $qa_link_type == 'form_link' ): ?>
+		<?php 
+		$popup_contact_title = get_option( 'options_default_flex_form_title' );
+		$popup_contact_subtext = get_option( 'options_default_flex_form_subtitle' );
+		?>
+		<div style="display: none;max-width: 700px;" id="popop-form-<?php echo $row;?>-<?php echo $count;?>" class="button-popop-form">
+			<div class="button-popop-form-wrap">
+				<div class="button-popop-form-row">
+					<div class="button-popop-form-col form-image col-xs-12">
+						<?php if( $popup_contact_title ) { ?>
+						<div class="contact-title">
+							<div class="popup_contact_title"><?php echo $popup_contact_title; ?></div>
+						</div>
+						<?php } ?>
+						<?php if( $popup_contact_subtext ) { ?>
+						<div class="contact-title">
+							<div class="popup_contact_subtext"><?php echo $popup_contact_subtext; ?></div>
+						</div>
+						<?php } ?>
+						<div class="contact-form-page">
+							<div class="full_form_id">
+								<div class="full_form_id_wrap">
+									<?php echo do_shortcode( '[contact-form-7 id="'.$qa_form_link.'" ]' ); ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php endif; ?>
+				
 	</section>
 	
 	<?php if( $qa_accordion_style == 'tabs' ) { ?>	
